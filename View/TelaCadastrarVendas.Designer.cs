89b9d2b0574159b648bb4fab67f9ecb3acb3a25c
INGRIDY DA SILVA FERREIRA GAMA <ingridy.sfgama@senacsp.edu.br>
2022-06-30 17:10:23 -0300

    Adicionar arquivos de projeto.

---
+++ View/TelaCadastrarVendas.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace ProjetoAgenciaTI11T.View
+{
+    partial class TelaCadastrarVendas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxValor = new System.Windows.Forms.TextBox();
+            this.tbxCliente = new System.Windows.Forms.TextBox();
+            this.tbxFuncionario = new System.Windows.Forms.TextBox();
+            this.tbxPacote = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(171, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastro de Vendas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Código do Cliente:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 15);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Código do Funcionário:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 293);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Código do Pacote:";
+            // 
+            // tbxValor
+            // 
+            this.tbxValor.Location = new System.Drawing.Point(150, 117);
+            this.tbxValor.Name = "tbxValor";
+            this.tbxValor.Size = new System.Drawing.Size(179, 23);
+            this.tbxValor.TabIndex = 5;
+            // 
+            // tbxCliente
+            // 
+            this.tbxCliente.Location = new System.Drawing.Point(150, 174);
+            this.tbxCliente.Name = "tbxCliente";
+            this.tbxCliente.Size = new System.Drawing.Size(179, 23);
+            this.tbxCliente.TabIndex = 6;
+            // 
+            // tbxFuncionario
+            // 
+            this.tbxFuncionario.Location = new System.Drawing.Point(150, 228);
+            this.tbxFuncionario.Name = "tbxFuncionario";
+            this.tbxFuncionario.Size = new System.Drawing.Size(179, 23);
+            this.tbxFuncionario.TabIndex = 7;
+            // 
+            // tbxPacote
+            // 
+            this.tbxPacote.Location = new System.Drawing.Point(150, 290);
+            this.tbxPacote.Name = "tbxPacote";
+            this.tbxPacote.Size = new System.Drawing.Size(179, 23);
+            this.tbxPacote.TabIndex = 8;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(234, 366);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 9;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // TelaCadastrarVendas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 450);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.tbxPacote);
+            this.Controls.Add(this.tbxFuncionario);
+            this.Controls.Add(this.tbxCliente);
+            this.Controls.Add(this.tbxValor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "TelaCadastrarVendas";
+            this.Text = "TelaCadastrarVendas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxValor;
+        private System.Windows.Forms.TextBox tbxCliente;
+        private System.Windows.Forms.TextBox tbxFuncionario;
+        private System.Windows.Forms.TextBox tbxPacote;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
